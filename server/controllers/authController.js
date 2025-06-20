216328ccf1c11e0313f0cba55a6322cd73557473
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-06-20 11:21:38 +0530

    Update

---
+++ server/controllers/authController.js
@@ -0,0 +1,19 @@
+const authController = {
+    signup: async(req, res) => {
+        try{
+            const {
+                username,
+                email,
+                password
+            } = req.body
+
+
+            
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+};
+
+module.exports = authController;
